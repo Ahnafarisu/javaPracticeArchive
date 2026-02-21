90d7419acde2a6cdf1cfb18146305658b9b8dc36
Arisu <alifsiddique.2006@gmail.com>
2026-02-21 22:12:05 +0600

    Add calculation logic in calculation.java
    Implement basic calculations and print result.

---
+++ calculation.java
@@ -0,0 +1,18 @@
+public class calculation {
+    public static void main(String[] args){
+        //Coding Task 2
+        int a = 2;
+        int b = 5;
+        int c = 8;
+        int e = 2 * b;
+        int f = c - a;
+        int g = f/3;
+        int d = e * g + 7;
+        System.out.println(d);
+
+
+
+
+
+    }
+}
